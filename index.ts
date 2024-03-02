f9769fcd9b14bf444f47dda3cd3c8f601ce440f0
Dylan Tompkins <dltompki@outlook.com>
2024-03-01 16:27:06 -0800

    it actually runs

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Response } from "express";
+import express, { Express, Response, Request } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -9,6 +9,18 @@ const port = process.env.PORT || 3000;
 app.get("/", (_, res: Response) => {
   res.send("Hello World!");
 });
+
+function echo(req: Request, res: Response) {
+  res.send(req);
+}
+
+app.get("/bathrooms", echo);
+app.get("/bathrooms/:bathroomId", echo);
+app.post("/bathrooms", echo);
+app.get("/bathrooms/:bathroomId/reviews", echo);
+app.post("/bathroom/:bathroomId/reviews", echo);
+app.get("/bathrooms/:bathroomId/tags", echo);
+app.post("/bathroom/:bathroomId/tags", echo);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);

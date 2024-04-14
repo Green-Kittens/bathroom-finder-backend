f8db3171870fec0dc06b9a2e6755cfdb639e0663
Jonas <j.schiessl2001@gmail.com>
2024-04-13 17:56:43 -0700

    error fixed

--- src/middleware/errorMiddleware.ts
+++ src/middleware/errorMiddleware.ts
@@ -1,10 +1,6 @@
-import { Request, Response} from "express";
+import { Request, Response } from "express";
 
-export function handleError(
-  err: Error,
-  req: Request,
-  res: Response
-) {
+export function handleError(err: Error, req: Request, res: Response) {
   console.error(err);
   res.status(500).send("An internal error occurred");
 }

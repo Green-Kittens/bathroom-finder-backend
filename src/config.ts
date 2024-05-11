9e3f26c0acc7bb3fa13482f3ac473fc4c6ef3944
Jonas <j.schiessl2001@gmail.com>
2024-05-10 21:39:28 -0700

    files

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,4 @@ dotenv.config();
 export const config = {
   port: process.env.PORT || 3000,
   databaseUrl: process.env.DATABASE_URL || "",
-  workosApiKey: process.env.WORKOS_API_KEY || "",
-  workosClientId: process.env.WORKOS_CLIENT_ID || "",
-  workosCookiePassword: process.env.WORKOS_COOKIE_PASSWORD || "",
 };

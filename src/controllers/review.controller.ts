11d4b53359e06e0ea196ce40947f641293d2e028
Jonas <j.schiessl2001@gmail.com>
2024-06-07 20:16:39 -0700

    feat: Refactor review controller to use arrow function syntax
    The review controller has been refactored to use arrow function syntax for better code readability and consistency. This change aligns with the recent repository commits that indicate the use of arrow functions in other controllers.

--- src/controllers/review.controller.ts
+++ src/controllers/review.controller.ts
@@ -2,7 +2,11 @@ import Review from "../models/review.model.js";
 import { Request, Response, NextFunction } from "express";
 
 // Create a new review
-export const createReview = async (req: Request, res: Response, next: NextFunction) => {
+export const createReview = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const newReview = new Review(req.body);
     await newReview.save();
@@ -13,7 +17,11 @@ export const createReview = async (req: Request, res: Response, next: NextFuncti
 };
 
 // Get all reviews
-export const getAllReviews = async (_: Request, res: Response, next: NextFunction) => {
+export const getAllReviews = async (
+  _: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const reviews = await Review.find({});
     res.status(200).send(reviews);
@@ -23,7 +31,11 @@ export const getAllReviews = async (_: Request, res: Response, next: NextFunctio
 };
 
 // Get a review by ID
-export const getReviewById = async (req: Request, res: Response, next: NextFunction) => {
+export const getReviewById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const review = await Review.findById(req.params.id);
     if (!review) {
@@ -36,7 +48,11 @@ export const getReviewById = async (req: Request, res: Response, next: NextFunct
 };
 
 // Update a review
-export const updateReview = async (req: Request, res: Response, next: NextFunction) => {
+export const updateReview = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const review = await Review.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
@@ -52,7 +68,11 @@ export const updateReview = async (req: Request, res: Response, next: NextFuncti
 };
 
 // Delete a review
-export const deleteReview = async (req: Request, res: Response, next: NextFunction) => {
+export const deleteReview = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const review = await Review.findByIdAndDelete(req.params.id);
     if (!review) {
